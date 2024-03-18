5e03f8740005b71bc5740608e8cc2bf814f9ae9d
Moballigh5225 <moballigh5225@gmail.com>
2024-03-19 02:27:02 +0530

    education fix

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2016 - 2022",
+    institution: "Lords Institute of Engineering & Technology",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    title: "Higher Secondary School",
+    duration: "2014 - 2016",
+    institution: "Narayana Junior College",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
-  }
-]
+    title: "High School(xth)",
+    duration: "2014",
+    institution: "Shatabdi Public School",
+  },
+];
