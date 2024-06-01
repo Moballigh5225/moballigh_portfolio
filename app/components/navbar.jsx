12ce23c8462948eeb0950dc0f5e52e1b128ffcd2
Moballigh5225 <moballigh5225@gmail.com>
2024-06-01 11:58:25 +0530

    minor changes

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -55,16 +55,7 @@ function Navbar() {
               </div>
             </a>
           </li>
-          {/* <li>
-            <a
-              className="block px-4 py-2 no-underline outline-none hover:no-underline"
-              href="#blogs"
-            >
-              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
-                BLOGS
-              </div>
-            </a>
-          </li> */}
+
           <li>
             <a
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
